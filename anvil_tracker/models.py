c4b50d026e5a13f0b30ed651481b650529d41f8d
Adrienne Stilp <amstilp@uw.edu>
2022-02-14 11:35:58 -0800

    Add initial model names and docstrings for the app

--- anvil_tracker/models.py
+++ anvil_tracker/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Investigator(models.Model):
+    """A model to store information about AnVIL investigators."""
+
+
+class Group(models.Model):
+    """A model to store information about AnVIL groups."""
+
+
+class Workspace(models.Model):
+    """A model to store inromation about AnVIL workspaces."""
+
+
+class GroupMembership(models.Model):
+    """A model to store which investigators are in a group."""
+
+
+class WorkspaceGroups(models.Model):
+    """A model to store which groups have access to a workspace."""
