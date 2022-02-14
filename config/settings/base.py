5cc18d27fb8dc8446a5d32bb4290451651c0c38a
Adrienne Stilp <amstilp@uw.edu>
2022-02-14 13:27:56 -0800

    Add app to installed apps for development

--- config/settings/base.py
+++ config/settings/base.py
@@ -83,6 +83,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "example_site.users",
     # Your stuff: custom apps go here
+    "anvil_tracker",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
