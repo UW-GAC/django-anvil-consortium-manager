14332eeccd1666f625319335e5e8a6c62b049bd9
Adrienne Stilp <amstilp@uw.edu>
2022-02-15 13:32:41 -0800

    Fix CI linting

--- anvil_tracker/apps.py
+++ anvil_tracker/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class AnvilTrackerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'anvil_tracker'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "anvil_tracker"
