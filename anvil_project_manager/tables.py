4fbe8f952a033ab74c0be4c318b23c2caf83e8cd
Adrienne Stilp <amstilp@uw.edu>
2022-02-23 15:42:38 -0800

    Add a service account indicator to group membership table

--- anvil_project_manager/tables.py
+++ anvil_project_manager/tables.py
@@ -51,6 +51,7 @@ class GroupMembershipTable(tables.Table):
         "anvil_project_manager:accounts:detail",
         args=[tables.utils.A("account__pk")],
     )
+    is_service_account = tables.BooleanColumn(accessor="account.is_service_account")
     group = tables.LinkColumn(
         "anvil_project_manager:groups:detail", args=[tables.utils.A("group__pk")]
     )
@@ -58,7 +59,7 @@ class GroupMembershipTable(tables.Table):
 
     class Meta:
         models = models.GroupMembership
-        fields = ("pk", "account", "group", "role")
+        fields = ("pk", "account", "is_service_account", "group", "role")
 
 
 class WorkspaceGroupAccessTable(tables.Table):
