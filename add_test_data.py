b078b72cb5e7d4669a6f9052ed95500e85335e58
Adrienne Stilp <amstilp@uw.edu>
2022-02-24 09:41:03 -0800

    Add a GroupGroupMembership relationship to test data script

--- add_test_data.py
+++ add_test_data.py
@@ -7,5 +7,8 @@ accounts = factories.AccountFactory.create_batch(5)
 groups = factories.GroupFactory.create_batch(5)
 workspaces = factories.WorkspaceFactory.create_batch(5)
 
-factories.GroupAccountMembershipFactory.create(group=groups[0], account=accounts[0])
-factories.WorkspaceGroupAccessFactory.create(workspace=workspaces[1], group=groups[0])
+factories.GroupGroupMembershipFactory.create(
+    parent_group=groups[0], child_group=groups[1]
+)
+factories.GroupAccountMembershipFactory.create(group=groups[1], account=accounts[0])
+factories.WorkspaceGroupAccessFactory.create(workspace=workspaces[1], group=groups[1])
