12ee238b471346335c3394bef193ac0d8aa3ed3f
Adrienne Stilp <amstilp@uw.edu>
2022-02-24 16:29:37 -0800

    Add an api source file with a basic function to get a session
    This is very basic and likely not how we want to make API calls in
    the end, but it will work for now.

---
+++ anvil_project_manager/anvil_api.py
@@ -0,0 +1,23 @@
+# Python firecloud API bindings
+# https://github.com/broadinstitute/fiss/blob/master/firecloud/api.py
+#
+# These don't work with python3.10, don't allow us to do everything we need,
+# and have some dependency resolution issues with this project. Therefore, we'll
+# have to reproduce some of the API to make the calls we would like to make. Alas.
+import google.auth
+from google.auth.transport.requests import AuthorizedSession
+
+# Will eventually want to make this a setting.
+ANVIL_API_ENDPOINT = "https://api.firecloud.org/api/"
+
+
+class AnVILAPISession(AuthorizedSession):
+    def __init__(self, endpoint=ANVIL_API_ENDPOINT):
+        self.credentials = google.auth.default(
+            [
+                "https://www.googleapis.com/auth/userinfo.profile",
+                "https://www.googleapis.com/auth/userinfo.email",
+            ]
+        )[0]
+        super().__init__(self.credentials)
+        self.endpoint = endpoint
