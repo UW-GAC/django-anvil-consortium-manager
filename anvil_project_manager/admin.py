e69f231e8faf11a243bc2f996a5c32fbbabdc5c4
Adrienne Stilp <amstilp@uw.edu>
2022-03-15 11:05:08 -0700

    Register app models in the admin

--- anvil_project_manager/admin.py
+++ anvil_project_manager/admin.py
@@ -1,1 +1,14 @@
-# Register your models here.
+"""Admin classes for the anvil_project_manager app."""
+
+from django.contrib import admin
+
+from . import models
+
+admin.site.register(models.BillingProject)
+admin.site.register(models.Account)
+admin.site.register(models.Group)
+admin.site.register(models.Workspace)
+admin.site.register(models.WorkspaceAuthorizationDomain)
+admin.site.register(models.GroupGroupMembership)
+admin.site.register(models.GroupAccountMembership)
+admin.site.register(models.WorkspaceGroupAccess)
