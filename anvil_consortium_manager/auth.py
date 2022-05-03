7d2fdd69df40b5152ee5c332a4ff00ae103beca3
Adrienne Stilp <amstilp@uw.edu>
2022-05-03 09:13:47 -0700

    Add mixin for view permission required

--- anvil_consortium_manager/auth.py
+++ anvil_consortium_manager/auth.py
@@ -4,6 +4,15 @@ from django.contrib.contenttypes.models import ContentType
 from .models import AnvilProjectManagerAccess
 
 
+class AnvilConsortiumManagerViewRequired(PermissionRequiredMixin):
+    """Anvil global app view permission required mixin"""
+
+    def get_permission_required(self):
+        apm_content_type = ContentType.objects.get_for_model(AnvilProjectManagerAccess)
+        perm_required = f"{apm_content_type.app_label}.{AnvilProjectManagerAccess.VIEW_PERMISSION_CODENAME}"
+        return (perm_required,)
+
+
 class AnvilConsortiumManagerEditRequired(PermissionRequiredMixin):
     """Anvil global app edit permission required mixin"""
 
