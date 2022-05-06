420769fbc84ae0389880ee5813ecee17bafc450f
Adrienne Stilp <amstilp@uw.edu>
2022-05-06 14:35:47 -0700

    Move settings file to example_site
    Remove the config directory since it is now empty

--- example_site/settings.py
+++ example_site/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 import environ
 
-ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
+ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent
 # example_site/
 APPS_DIR = ROOT_DIR / "example_site"
 env = environ.Env()
