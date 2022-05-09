4b7bea383d30d7f720a3144c9c4fa60d5acac18e
Adrienne Stilp <amstilp@uw.edu>
2022-05-09 10:34:13 -0700

    Use a default settings module in runtests script

--- runtests.py
+++ runtests.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 import sys
 
 import django
@@ -6,6 +7,12 @@ from django.conf import settings
 from django.test.utils import get_runner
 
 if __name__ == "__main__":
+    try:
+        settings_module = os.environ["DJANGO_SETTINGS_MODULE"]
+    except KeyError:
+        settings_module = "anvil_consortium_manager.tests.settings.test"
+        os.environ["DJANGO_SETTINGS_MODULE"] = settings_module
+    print("Running with settings " + settings_module)
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
