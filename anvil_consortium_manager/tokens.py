95c2cde5518069c5128d7490f2f2bf466e0b9465
Wienwipa Kirdpoo <wkirdpoo@uw.edu>
2022-08-02 15:11:20 -0700

    Not using six to create token.

--- anvil_consortium_manager/tokens.py
+++ anvil_consortium_manager/tokens.py
@@ -1,10 +1,9 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils import six
 
 class AccountLinkVerificationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (
-            six.text_type(user.pk) + six.text_type(timestamp)
+            str(user.pk) + str(timestamp)
         )
 
 account_verification_token = AccountLinkVerificationTokenGenerator()
