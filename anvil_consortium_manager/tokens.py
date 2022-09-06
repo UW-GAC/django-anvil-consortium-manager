a62e2c558f097a1b94cab94df09f6d24f47dd073
Wienwipa Kirdpoo <wkirdpoo@uw.edu>
2022-09-06 03:30:35 -0700

    add email to token generator

--- anvil_consortium_manager/tokens.py
+++ anvil_consortium_manager/tokens.py
@@ -2,8 +2,8 @@ from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 
 class AccountLinkVerificationTokenGenerator(PasswordResetTokenGenerator):
-    def _make_hash_value(self, user, timestamp):
-        return str(user.pk) + str(timestamp)
+    def _make_hash_value(self, email_entry, timestamp):
+        return str(email_entry.pk) + str(timestamp) + str(email_entry.email)
 
 
 account_verification_token = AccountLinkVerificationTokenGenerator()
