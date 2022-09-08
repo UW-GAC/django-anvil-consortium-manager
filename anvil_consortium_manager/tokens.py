aeebec17a4e5f0f666b54faba8fe868a063dea81
Adrienne Stilp <amstilp@uw.edu>
2022-09-08 16:55:47 -0700

    Add docstring for tokens source

--- anvil_consortium_manager/tokens.py
+++ anvil_consortium_manager/tokens.py
@@ -2,6 +2,8 @@ from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 
 class AccountLinkVerificationTokenGenerator(PasswordResetTokenGenerator):
+    """Class to generate a token for a UserEmailEntry using the pk, the email, and a timestamp."""
+
     def _make_hash_value(self, email_entry, timestamp):
         return str(email_entry.pk) + str(timestamp) + str(email_entry.email)
 
