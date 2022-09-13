7699bd3d52b8960b83101aaa72cc2c21796bb8cf
Adrienne Stilp <amstilp@uw.edu>
2022-09-13 11:30:28 -0700

    Clean up exceptions file

--- anvil_consortium_manager/exceptions.py
+++ anvil_consortium_manager/exceptions.py
@@ -4,25 +4,17 @@
 class AnVILNotWorkspaceOwnerError(Exception):
     """Exception to be raised when the app account is not the owner of a workspace on AnVIL."""
 
-    pass
-
 
 class AnVILNotGroupAdminError(Exception):
     """Exception to be raised when the app account is not the admin of a group on AnVIL."""
-
-    pass
 
 
 class AnVILNotGroupMemberError(Exception):
     """Exception to be raised when the app account is not a member or admin of a group on AnVIL."""
 
-    pass
-
 
 class AnVILAlreadyImported(Exception):
     """Exception to be raised when an AnVIL resource has already been imported into Django."""
-
-    pass
 
 
 class AnVILRemoveAccountFromGroupError(Exception):
