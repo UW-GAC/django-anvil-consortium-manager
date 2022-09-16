a62d7ae3b8880426585cfd9ccedded0cd5cff007
Adrienne Stilp <amstilp@uw.edu>
2022-09-15 17:02:18 -0700

    Convert AnVILAuditResults to an abstract base class with an allowed_errors property

--- anvil_consortium_manager/anvil_audit.py
+++ anvil_consortium_manager/anvil_audit.py
@@ -1,8 +1,15 @@
 """Classes handling auditing of information in the Django database against AnVIL."""
 
+from abc import ABC, abstractproperty
 
-class AnVILAuditResults:
-    """Base class to store audit results from AnVIL."""
+
+class AnVILAuditResults(ABC):
+    """Abstract base class to store audit results from AnVIL."""
+
+    @abstractproperty
+    def allowed_errors(self):
+        """List specifying the list of allowed errors for this audit result class."""
+        ...
 
     def __init__(self):
         self.verified = set()
@@ -15,6 +22,8 @@ class AnVILAuditResults:
 
     def add_error(self, record, error):
         """Add a record from the app that has an error in the audit."""
+        if error not in self.allowed_errors:
+            raise ValueError("'{}' is not an allowed error.".format(error))
         if record in self.errors:
             self.errors[record].append(error)
         else:
@@ -47,12 +56,16 @@ class BillingProjectAuditResults(AnVILAuditResults):
     """Class to hold audit results for BillingProjects."""
 
     ERROR_NOT_IN_ANVIL = "Not in AnVIL"
+    # Set up allowed errors.
+    allowed_errors = ERROR_NOT_IN_ANVIL
 
 
 class AccountAuditResults(AnVILAuditResults):
     """Class to hold audit results for Accounts."""
 
     ERROR_NOT_IN_ANVIL = "Not in AnVIL"
+    # Set up allowed errors.
+    allowed_errors = ERROR_NOT_IN_ANVIL
 
 
 class ManagedGroupAuditResults(AnVILAuditResults):
@@ -61,6 +74,12 @@ class ManagedGroupAuditResults(AnVILAuditResults):
     ERROR_NOT_IN_ANVIL = "Not in AnVIL"
     ERROR_DIFFERENT_ROLE = "App has a different role in this group"
     ERROR_GROUP_MEMBERSHIP = "Group membership does not match in AnVIL"
+    # Set up allowed errors.
+    allowed_errors = (
+        ERROR_NOT_IN_ANVIL,
+        ERROR_DIFFERENT_ROLE,
+        ERROR_GROUP_MEMBERSHIP,
+    )
 
 
 class ManagedGroupMembershipAuditResults(AnVILAuditResults):
@@ -70,6 +89,13 @@ class ManagedGroupMembershipAuditResults(AnVILAuditResults):
     ERROR_ACCOUNT_MEMBER_NOT_IN_ANVIL = "Account not a member in AnVIL"
     ERROR_GROUP_ADMIN_NOT_IN_ANVIL = "Group not an admin in AnVIL"
     ERROR_GROUP_MEMBER_NOT_IN_ANVIL = "Group not a member in AnVIL"
+    # Set up allowed errors.
+    allowed_errors = (
+        ERROR_ACCOUNT_ADMIN_NOT_IN_ANVIL,
+        ERROR_ACCOUNT_MEMBER_NOT_IN_ANVIL,
+        ERROR_GROUP_ADMIN_NOT_IN_ANVIL,
+        ERROR_GROUP_MEMBER_NOT_IN_ANVIL,
+    )
 
 
 class WorkspaceAuditResults(AnVILAuditResults):
@@ -78,3 +104,9 @@ class WorkspaceAuditResults(AnVILAuditResults):
     ERROR_NOT_IN_ANVIL = "Not in AnVIL"
     ERROR_NOT_OWNER_ON_ANVIL = "Not an owner on AnVIL"
     ERROR_DIFFERENT_AUTH_DOMAINS = "Has different auth domains on AnVIL"
+    # Set up allowed errors.
+    allowed_errors = (
+        ERROR_NOT_IN_ANVIL,
+        ERROR_NOT_OWNER_ON_ANVIL,
+        ERROR_DIFFERENT_AUTH_DOMAINS,
+    )
