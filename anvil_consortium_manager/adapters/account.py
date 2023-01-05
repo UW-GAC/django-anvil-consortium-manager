a1e229e815e92bbd69ff8b48d29e81bf035a46f0
Adrienne Stilp <amstilp@uw.edu>
2023-01-05 11:53:31 -0800

    Add a method to return the account adapter based on settings

--- anvil_consortium_manager/adapters/account.py
+++ anvil_consortium_manager/adapters/account.py
@@ -2,6 +2,9 @@
 
 from abc import ABC
 
+from django.conf import settings
+from django.utils.module_loading import import_string
+
 
 class BaseAccountAdapter(ABC):
     """Base class to inherit when customizing the account adapter."""
@@ -12,3 +15,8 @@ class BaseAccountAdapter(ABC):
         This method can be extended to show informatiom from the user profile linked to the Account.
         """
         return str(account)
+
+
+def get_account_adapter():
+    adapter = import_string(settings.ANVIL_ACCOUNT_ADAPTER)
+    return adapter
