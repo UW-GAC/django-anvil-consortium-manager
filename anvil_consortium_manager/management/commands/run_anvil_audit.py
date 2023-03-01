b0b1d1a41dda10059be48a41951bcc0471f0cfd9
Adrienne Stilp <amstilp@uw.edu>
2023-03-01 11:20:28 -0800

    Add auditing for the BillingProject model
    Add a command allowing the user to specify which models to audit.
    If they specify "BillingProject", then run the billing project
    audit.

--- anvil_consortium_manager/management/commands/run_anvil_audit.py
+++ anvil_consortium_manager/management/commands/run_anvil_audit.py
@@ -1,9 +1,28 @@
+import json
+
 from django.core.management.base import BaseCommand
+
+from ... import models
 
 
 class Command(BaseCommand):
 
     help = """Management command to run an AnVIL audit."""
 
+    def add_arguments(self, parser):
+        parser.add_argument("models", nargs=1, type=str)
+
     def handle(self, *args, **options):
+
+        models_to_audit = options["models"]
+
+        # Billing projects.
+        if "BillingProject" in models_to_audit:
+            self.stdout.write("Running on BillingProjects...")
+            billing_project_results = models.BillingProject.anvil_audit()
+            report = json.dumps(
+                billing_project_results.to_json(include_verified=False), indent=2
+            )
+            self.stdout.write(report)
+
         self.stdout.write("Done!")
--- anvil_consortium_manager/tests/test_commands.py
+++ anvil_consortium_manager/tests/test_commands.py
@@ -2,15 +2,56 @@
 
 from io import StringIO
 
+import responses
 from django.core.management import call_command
 from django.test import TestCase
 
+from .. import anvil_audit
+from . import factories
+from .utils import AnVILAPIMockTestMixin
 
-class RunAnvilAuditTest(TestCase):
+
+class RunAnvilAuditTest(AnVILAPIMockTestMixin, TestCase):
     """Tests for the run_anvil_audit command"""
+
+    def get_api_url_billing_project(self, billing_project_name):
+        return (
+            self.api_client.rawls_entry_point
+            + "/api/billing/v2/"
+            + billing_project_name
+        )
 
     def test_command_output_no_instances(self):
         """Test command output."""
         out = StringIO()
-        call_command("run_anvil_audit", stdout=out)
+        call_command("run_anvil_audit", "BillingProject", stdout=out)
+        self.assertIn("Done!", out.getvalue())
+
+    def test_command_output_with_billing_project_ok(self):
+        """Test command output."""
+        billing_project = factories.BillingProjectFactory.create()
+        # Add a response.
+        api_url = self.get_api_url_billing_project(billing_project.name)
+        self.anvil_response_mock.add(responses.GET, api_url, status=200)
+        out = StringIO()
+        call_command("run_anvil_audit", "BillingProject", stdout=out)
+        self.assertIn("BillingProjects", out.getvalue())
+        self.assertIn(""""errors": []""", out.getvalue())
+        self.assertIn(""""not_in_app": []""", out.getvalue())
+        self.assertIn("Done!", out.getvalue())
+
+    def test_command_output_with_billing_project_not_ok(self):
+        """Test command output when BillingProject audit is not ok."""
+        billing_project = factories.BillingProjectFactory.create()
+        # Add a response.
+        api_url = self.get_api_url_billing_project(billing_project.name)
+        self.anvil_response_mock.add(
+            responses.GET, api_url, status=404, json={"message": "error"}
+        )
+        out = StringIO()
+        call_command("run_anvil_audit", "BillingProject", stdout=out)
+        self.assertIn("BillingProjects", out.getvalue())
+        self.assertIn(
+            anvil_audit.BillingProjectAuditResults.ERROR_NOT_IN_ANVIL, out.getvalue()
+        )
         self.assertIn("Done!", out.getvalue())
