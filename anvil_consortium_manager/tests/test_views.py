9ef2b5459d2de0d9c51485e3694535189bea06e6
Wienwipa Kirdpoo <wkirdpoo@uw.edu>
2023-09-19 16:39:57 -0700

    Added more tests for BillingProjectList filter view

--- anvil_consortium_manager/tests/test_views.py
+++ anvil_consortium_manager/tests/test_views.py
@@ -1005,8 +1005,18 @@ class BillingProjectListTest(TestCase):
         self.assertIn("table", response.context_data)
         self.assertEqual(len(response.context_data["table"].rows), 2)
 
-    def test_view_with_one_filtered_object(self):
-        factories.BillingProjectFactory.create(name="Billing project")
+    def test_view_with_filter_returns_all_objects(self):
+        factories.BillingProjectFactory.create(name="Billing_project")
+        factories.BillingProjectFactory.create(name="Project")
+        self.client.force_login(self.user)
+        url = resolve_url(self.get_url() + "?name__icontains=pro")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("table", response.context_data)
+        self.assertEqual(len(response.context_data["table"].rows), 2)
+
+    def test_view_with_filter_returns_one_object(self):
+        factories.BillingProjectFactory.create(name="Billing_project")
         factories.BillingProjectFactory.create(name="Project")
         self.client.force_login(self.user)
         url = resolve_url(self.get_url() + "?name__icontains=bill")
@@ -1014,6 +1024,16 @@ class BillingProjectListTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn("table", response.context_data)
         self.assertEqual(len(response.context_data["table"].rows), 1)
+
+    def test_view_with_filter_return_no_object(self):
+        factories.BillingProjectFactory.create(name="Billing_project")
+        factories.BillingProjectFactory.create(name="Project")
+        self.client.force_login(self.user)
+        url = resolve_url(self.get_url() + "?name__icontains=abc")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("table", response.context_data)
+        self.assertEqual(len(response.context_data["table"].rows), 0)
 
 
 class BillingProjectAutocompleteTest(TestCase):
