3c885ea59865db679bf9fcd8efc8edae84ef4180
Wienwipa Kirdpoo <wkirdpoo@uw.edu>
2023-09-19 16:45:26 -0700

    Rename BillingProjectFilter to BillingProjectListFilter

--- anvil_consortium_manager/filters.py
+++ anvil_consortium_manager/filters.py
@@ -3,7 +3,7 @@ from django_filters import FilterSet
 from . import models
 
 
-class BillingProjectFilter(FilterSet):
+class BillingProjectListFilter(FilterSet):
     class Meta:
         model = models.BillingProject
         fields = {"name": ["icontains"]}
