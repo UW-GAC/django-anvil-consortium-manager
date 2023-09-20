bc1530c6c86933204fe89a695a6abaaf7a2c23aa
Wienwipa Kirdpoo <wkirdpoo@uw.edu>
2023-09-20 14:37:38 -0700

    Define filter for AccountList Add AccountList filter to AccountList, AccountActiveList and AccountInactiveList views Add filter form to account_list template Add tests for  AccountList, AccountActiveList and AccountInactiveList views

--- anvil_consortium_manager/filters.py
+++ anvil_consortium_manager/filters.py
@@ -3,6 +3,12 @@ from django_filters import FilterSet
 from . import models
 
 
+class AccountListFilter(FilterSet):
+    class Meta:
+        model = models.Account
+        fields = {"email": ["icontains"]}
+
+
 class BillingProjectListFilter(FilterSet):
     class Meta:
         model = models.BillingProject
