75de886d6afc08ca38c7f3219d49521237b5d86d
Wienwipa Kirdpoo <wkirdpoo@uw.edu>
2023-09-19 17:36:49 -0700

    Define filter for MangagedGroupList Add filter to ManageGroupList view Add the FilterSet to managedgroup_list template Add tests

--- anvil_consortium_manager/filters.py
+++ anvil_consortium_manager/filters.py
@@ -7,3 +7,9 @@ class BillingProjectListFilter(FilterSet):
     class Meta:
         model = models.BillingProject
         fields = {"name": ["icontains"]}
+
+
+class ManagedGroupListFilter(FilterSet):
+    class Meta:
+        model = models.ManagedGroup
+        fields = {"name": ["icontains"]}
