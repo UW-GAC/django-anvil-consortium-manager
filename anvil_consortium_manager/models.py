64553bf3901ea91230d30417c4aed053d6066772
Adrienne Stilp <amstilp@uw.edu>
2023-11-06 15:42:30 -0800

    Rename permissions in models and migrations
    Add a migration to create the new permissions, and then a data
    migration to rename the original permissions (if they exist) to the
    new permissions. This appears to work ok. We can't easily test this
    because the django-test-migrations package mutes signals during the
    migrations, so no ContentTypes or Permissions end up being created.

--- anvil_consortium_manager/models.py
+++ anvil_consortium_manager/models.py
@@ -21,8 +21,8 @@ class AnVILProjectManagerAccess(models.Model):
     """A meta model used to define app level permissions"""
 
     STAFF_EDIT_PERMISSION_CODENAME = "anvil_project_manager_staff_edit"
+    STAFF_VIEW_PERMISSION_CODENAME = "anvil_project_manager_staff_view"
     VIEW_PERMISSION_CODENAME = "anvil_project_manager_view"
-    LIMITED_VIEW_PERMISSION_CODENAME = "anvil_project_manager_limited_view"
     ACCOUNT_LINK_PERMISSION_CODENAME = "anvil_project_manager_account_link"
 
     class Meta:
@@ -34,15 +34,15 @@ class AnVILProjectManagerAccess(models.Model):
         default_permissions = ()
 
         permissions = [
-            ("anvil_project_manager_staff_edit", "AnVIL Project Manager Staff Edit Permission"),
-            ("anvil_project_manager_view", "AnVIL Project Manager View Permission"),
+            ("anvil_consortium_manager_staff_edit", "AnVIL Consortium Manager Staff Edit Permission"),
+            ("anvil_consortium_manager_staff_view", "AnVIL Consortium Manager Staff View Permission"),
             (
-                "anvil_project_manager_account_link",
-                "AnVIL Project Manager Account Link Permission",
+                "anvil_consortium_manager_account_link",
+                "AnVIL Consortium Manager Account Link Permission",
             ),
             (
-                "anvil_project_manager_limited_view",
-                "AnVIL Project Manager Limited View Permission",
+                "anvil_consortium_manager_view",
+                "AnVIL Consortium Manager View Permission",
             ),
         ]
 
