49f5682c0fa903ad31845cb3ee027405377b74f3
Adrienne Stilp <amstilp@uw.edu>
2025-01-08 16:28:18 -0800

    Add a model to track ignored WorkspaceGroupSharing audit results
    Add an abtract model that stores standard information for ignored
    audit results (e.g., who ignored it and a note). Use this abstract
    model to define the existing IgnoredManagedGroupMembership model
    and the new IgnoredWorkspaceGroupSharing model.

--- anvil_consortium_manager/auditor/models.py
+++ anvil_consortium_manager/auditor/models.py
@@ -5,7 +5,17 @@ from django_extensions.db.models import TimeStampedModel
 from simple_history.models import HistoricalRecords
 
 
-class IgnoredManagedGroupMembership(TimeStampedModel):
+class IgnoredAuditResult(TimeStampedModel):
+    added_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT, help_text="User who added the record to this table."
+    )
+    note = models.TextField(help_text="Note about why this email is being ignored.")
+
+    class Meta:
+        abstract = True
+
+
+class IgnoredManagedGroupMembership(IgnoredAuditResult):
     """A model to store audit records that can be ignored during a ManagedGroupMembership audit.
 
     Right now this model is intended to track "not in app" records that can be ignored."""
@@ -16,10 +26,6 @@ class IgnoredManagedGroupMembership(TimeStampedModel):
         help_text="Group where email should be ignored.",
     )
     ignored_email = models.EmailField(help_text="Email address to ignore.")
-    added_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.PROTECT, help_text="User who added the record to this table."
-    )
-    note = models.TextField(help_text="Note about why this email is being ignored.")
     history = HistoricalRecords()
 
     class Meta:
@@ -45,3 +51,43 @@ class IgnoredManagedGroupMembership(TimeStampedModel):
                 "email": self.ignored_email,
             },
         )
+
+
+class IgnoredWorkspaceGroupSharing(IgnoredAuditResult):
+    """A model to store audit records that can be ignored during a WorkspaceGroupSharing audit.
+
+    Right now this model is intended to track "not in app" records that can be ignored."""
+
+    workspace = models.ForeignKey(
+        "anvil_consortium_manager.Workspace",
+        on_delete=models.CASCADE,
+        help_text="Workspace where email should be ignored.",
+    )
+    ignored_email = models.EmailField(help_text="Email address to ignore.")
+    history = HistoricalRecords()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["workspace", "ignored_email"], name="unique_workspace_ignored_email")
+        ]
+
+    def __str__(self):
+        return "{workspace} sharing: ignoring {email}".format(workspace=self.workspace, email=self.ignored_email)
+
+    def save(self, *args, **kwargs):
+        """Save method to set the email address to lowercase before saving."""
+        self.ignored_email = self.ignored_email.lower()
+        return super().save(*args, **kwargs)
+
+    # def get_absolute_url(self):
+    #     """Get the absolute url for this object.
+
+    #     Returns:
+    #         str: The absolute url for the object."""
+    #     return reverse(
+    #         "anvil_consortium_manager:auditor:managed_groups:membership:by_group:ignored:detail",
+    #         kwargs={
+    #             "slug": self.group.name,
+    #             "email": self.ignored_email,
+    #         },
+    #     )
--- anvil_consortium_manager/auditor/tests/test_models.py
+++ anvil_consortium_manager/auditor/tests/test_models.py
@@ -1,5 +1,9 @@
 from django.db.utils import IntegrityError
 
+from anvil_consortium_manager.tests.factories import (
+    ManagedGroupFactory,
+    WorkspaceFactory,
+)
 from anvil_consortium_manager.tests.utils import TestCase  # Redefined to work with Django < 4.2 and Django=4.2.
 
 from .. import models
@@ -11,7 +15,7 @@ class IgnoredManagedGroupMembershipTest(TestCase):
 
     def test_model_saving(self):
         """Creation using the model constructor and .save() works."""
-        group = factories.ManagedGroupFactory.create()
+        group = ManagedGroupFactory.create()
         user = factories.UserFactory.create()
         instance = models.IgnoredManagedGroupMembership(
             group=group,
@@ -51,7 +55,7 @@ class IgnoredManagedGroupMembershipTest(TestCase):
 
     def test_unique(self):
         # Cannot save the same record for the same group and email.
-        group = factories.ManagedGroupFactory.create()
+        group = ManagedGroupFactory.create()
         email = "email@example.com"
         instance_1 = factories.IgnoredManagedGroupMembershipFactory.build(
             group=group,
@@ -69,7 +73,7 @@ class IgnoredManagedGroupMembershipTest(TestCase):
 
     def test_unique_case_insensitive(self):
         # Cannot save the same record for the same group and email.
-        group = factories.ManagedGroupFactory.create()
+        group = ManagedGroupFactory.create()
         email = "email@example.com"
         instance_1 = factories.IgnoredManagedGroupMembershipFactory.build(
             group=group,
@@ -79,6 +83,86 @@ class IgnoredManagedGroupMembershipTest(TestCase):
         instance_1.save()
         instance_2 = factories.IgnoredManagedGroupMembershipFactory.build(
             group=group,
+            ignored_email=email.upper(),
+            added_by=factories.UserFactory.create(),
+        )
+        with self.assertRaises(IntegrityError):
+            instance_2.save()
+
+
+class IgnoredWorkspaceGroupSharingTest(TestCase):
+    """Tests for the models.IgnoredWorkspaceGroupSharing model."""
+
+    def test_model_saving(self):
+        """Creation using the model constructor and .save() works."""
+        workspace = WorkspaceFactory.create()
+        user = factories.UserFactory.create()
+        instance = models.IgnoredWorkspaceGroupSharing(
+            workspace=workspace,
+            ignored_email="email@example.com",
+            added_by=user,
+            note="foo",
+        )
+        instance.save()
+        self.assertIsInstance(instance, models.IgnoredWorkspaceGroupSharing)
+
+    def test_str_method(self):
+        """The custom __str__ method returns the correct string."""
+        instance = factories.IgnoredWorkspaceGroupSharingFactory.create(
+            workspace__name="bar", workspace__billing_project__name="foo", ignored_email="email@example.com"
+        )
+        instance.save()
+        self.assertIsInstance(instance.__str__(), str)
+        self.assertEqual(instance.__str__(), "foo/bar sharing: ignoring email@example.com")
+
+    # def test_get_absolute_url(self):
+    #     """The get_absolute_url() method works."""
+    #     instance = factories.IgnoredWorkspaceGroupSharingFactory()
+    #     self.assertIsInstance(instance.get_absolute_url(), str)
+
+    def test_history(self):
+        """A simple history record is created when model is updated."""
+        obj = factories.IgnoredWorkspaceGroupSharingFactory.create()
+        # History was created.
+        self.assertEqual(obj.history.count(), 1)
+        # A new entry is created on update.
+        obj.note = "foo bar"
+        obj.save()
+        self.assertEqual(obj.history.count(), 2)
+        # An entry is created upon deletion.
+        obj.delete()
+        self.assertEqual(models.IgnoredWorkspaceGroupSharing.history.count(), 3)
+
+    def test_unique(self):
+        # Cannot save the same record for the same group and email.
+        workspace = WorkspaceFactory.create()
+        email = "email@example.com"
+        instance_1 = factories.IgnoredWorkspaceGroupSharingFactory.build(
+            workspace=workspace,
+            ignored_email=email,
+            added_by=factories.UserFactory.create(),
+        )
+        instance_1.save()
+        instance_2 = factories.IgnoredWorkspaceGroupSharingFactory.build(
+            workspace=workspace,
+            ignored_email=email,
+            added_by=factories.UserFactory.create(),
+        )
+        with self.assertRaises(IntegrityError):
+            instance_2.save()
+
+    def test_unique_case_insensitive(self):
+        # Cannot save the same record for the same group and email.
+        workspace = WorkspaceFactory.create()
+        email = "email@example.com"
+        instance_1 = factories.IgnoredWorkspaceGroupSharingFactory.build(
+            workspace=workspace,
+            ignored_email=email,
+            added_by=factories.UserFactory.create(),
+        )
+        instance_1.save()
+        instance_2 = factories.IgnoredWorkspaceGroupSharingFactory.build(
+            workspace=workspace,
             ignored_email=email.upper(),
             added_by=factories.UserFactory.create(),
         )
