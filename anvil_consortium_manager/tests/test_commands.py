1031389ce86d5bc7e27bde165d1a820cf2c02354
Adrienne Stilp <amstilp@uw.edu>
2026-01-21 14:38:55 -0800

    Add test for --models argument to convert_mariadb_uuid_fields

--- anvil_consortium_manager/tests/test_commands.py
+++ anvil_consortium_manager/tests/test_commands.py
@@ -4,7 +4,7 @@ from io import StringIO
 from unittest import skipUnless
 
 from django.conf import settings
-from django.core.management import call_command
+from django.core.management import CommandError, call_command
 from django.test import TransactionTestCase
 
 
@@ -16,3 +16,22 @@ class ConvertMariaDbUUIDFieldsTest(TransactionTestCase):
         out = StringIO()
         # Just make sure the command runs?
         call_command("convert_mariadb_uuid_fields", stdout=out)
+
+    @skipUnless(settings.DATABASES["default"]["ENGINE"] == "django.db.backends.mysql", "Only for MariaDB")
+    def test_convert_mariadb_uuid_fields_one_model(self):
+        """Test convert_mariadb_uuid_fields command with one model."""
+        # Add a response.
+        out = StringIO()
+        # Just make sure the command runs?
+        call_command("convert_mariadb_uuid_fields", "--models=Account", stdout=out)
+
+    @skipUnless(settings.DATABASES["default"]["ENGINE"] == "django.db.backends.mysql", "Only for MariaDB")
+    def test_convert_mariadb_uuid_fields_invalid_model(self):
+        """Test convert_mariadb_uuid_fields command with an invalid model."""
+        # Add a response.
+        out = StringIO()
+        with self.assertRaises(CommandError) as e:
+            # Call with the "--models=foo" so it goes through the argparse validation.
+            # Calling with models=["foo"] does not throw an exception.
+            call_command("convert_mariadb_uuid_fields", "--models=foo", stdout=out)
+        self.assertIn("invalid choice", str(e.exception))
